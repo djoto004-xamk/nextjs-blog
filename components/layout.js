1d55a2e8b337497fdf189411885d274822582c55
Joni Toikkanen <djoto004@edu.xamk.fi>
2023-01-01 10:05:21 +0200

    check

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Joni';
+const name = 'Joni Toikkanen';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
